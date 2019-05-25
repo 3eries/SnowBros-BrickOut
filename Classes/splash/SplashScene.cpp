d0cd3552d94929cae9cf40c66e83ddfa11509d8d
seongmin hwang <>
2019-05-25 14:57:38 +0900

    [added] 앱 실행 시 시리즈 로고 출력

--- Classes/splash/SplashScene.cpp
+++ Classes/splash/SplashScene.cpp
@@ -97,88 +97,13 @@ void SplashScene::replaceMainScene() {
  */
 void SplashScene::initLaunchImage() {
 
-    SBDirector::postDelayed(this, [=]() {
+    auto logoView = LogoView::create();
+    addChild(logoView);
+    
+    logoView->setOnFinishedListener([=]() {
         this->launchImageFinished();
-    }, 1.0f, true);
-    
-    return;
-    
-    // 시리즈 로고는 30분동안 재노출 하지 않음
-    double now = SBSystemUtils::getCurrentTimeSeconds();
-    double viewTime = UserDefault::getInstance()->getDoubleForKey(UserDefaultKey::SPLASH_LOGO_VIEW_TIME, 0);
-    double dt = now - viewTime;
-    
-    CCLOG("SplashScene::initLaunchImage logo view dt: %f", dt);
-    
-    // 30분 경과, 로고 출력
-    if( dt >= (60*30) ) {
-        UserDefault::getInstance()->setDoubleForKey(UserDefaultKey::SPLASH_LOGO_VIEW_TIME, now);
-        UserDefault::getInstance()->flush();
-        
-        auto logoView = LogoView::create();
-        addChild(logoView);
-        
-        logoView->setOnFinishedListener([=]() {
-            this->launchImageFinished();
-        });
-        logoView->run();
-    }
-    // 명언
-    else {
-        logoMode = false;
-        
-        auto layer = SBNodeUtils::createZeroSizeNode();
-        addChild(layer);
-        
-        // bg
-        // layer->addChild(LayerColor::create(Color4B::BLACK));
-        
-        // famous saying
-        auto famousSaying = FunnyHelper::getRandomFamousSaying();
-        
-        auto saying = Label::createWithTTF(famousSaying.saying, FONT_FAMOUS_SAYING, 140,
-                                           Size(SB_WIN_SIZE.width*0.9f, 0),
-                                           TextHAlignment::CENTER, TextVAlignment::CENTER);
-        saying->setColor(Color3B::WHITE);
-        saying->setAnchorPoint(ANCHOR_MB);
-        saying->setPosition(Vec2MC(0, 40));
-        layer->addChild(saying);
-        
-        auto person = Label::createWithTTF("-" + famousSaying.person + "-", FONT_FAMOUS_SAYING, 110);
-        person->setColor(Color3B::WHITE);
-        person->setAnchorPoint(ANCHOR_MT);
-        person->setPosition(Vec2MC(0, -20));
-        layer->addChild(person);
-        
-        // 명언 텍스트 연출
-        {
-            saying->setOpacity(0);
-            saying->runAction(FadeIn::create(1.0f));
-        }
-        
-        // 인물 텍스트 연출
-        {
-            person->setOpacity(0);
-            
-            auto delay = DelayTime::create(1.5f);
-            auto fadeIn = FadeIn::create(1.0f);
-            person->runAction(Sequence::create(delay, fadeIn, nullptr));
-        }
-        
-        // 명언 fade out
-        {
-            SBNodeUtils::recursiveCascadeOpacityEnabled(layer, true);
-            
-            auto delay = DelayTime::create(3.5f);
-            auto fadeOut = FadeOut::create(0.5f);
-            layer->runAction(Sequence::create(delay, fadeOut, nullptr));
-        }
-        
-        // 완료 스케줄러
-        scheduleOnce([=](float dt) {
-            this->launchImageFinished();
-        }, LAUNCH_IMAGE_MIN_DURATION, LAUNCH_IMAGE_SCHEDULER);
-    }
+    });
+    logoView->run();
 }
 
 /**
